45e534939421bffc3ebf0b0bd05622403ed14b44
DeLucca990 <pedruca2001@gmail.com>
2025-02-04 23:57:06 -0300

    feat: accordion on AddProduct page

--- src/components/addProducToCatalog/AddProduct.tsx
+++ src/components/addProducToCatalog/AddProduct.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { useToast } from "@/hooks/use-toast"
+import { useToast } from "@/hooks/use-toast";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { addProduct, addProductsFile } from "@/api/endpoints";
 import LoadingSpinner from "../LoadingSpinner";
 import Header from "../Header";
@@ -36,7 +42,12 @@ const AddProductPage = () => {
         centro_custo: formData.centro_custo,
       });
       handleSuccessToast("Produto adicionado com sucesso!");
-      setFormData({ nome_produto: "", codigo_produto: "", unidade: "", centro_custo: "" });
+      setFormData({
+        nome_produto: "",
+        codigo_produto: "",
+        unidade: "",
+        centro_custo: "",
+      });
     } catch (error: any) {
       if (error.response && error.response.status === 400) {
         handleWarningToast("Produto já cadastrado.");
@@ -64,12 +75,11 @@ const AddProductPage = () => {
       setFile(null);
     } catch (error: any) {
       console.error("Erro ao enviar arquivo de produtos:", error);
-      if (error.response.status === 400 && error.response.data.detail === "A planilha deve conter as colunas: Código Produto/Material, Nome Produto/Especificação e Centro de Custo") {
-        handleWarningToast("A planilha deve conter as colunas: 'Código Produto ou Material', 'Nome Produto ou Especificação' e 'Centro de Custo'");
+      if (error.response.status === 400 && error.response.data.detail === "A planilha deve conter as colunas: Código Produto/Material, Nome Produto/Especificação, Unidade e Centro de Custo") {
+        handleWarningToast("A planilha deve conter as colunas: 'Código Produto ou Material', 'Nome Produto ou Especificação', 'Unidade' e 'Centro de Custo'");
         return;
       }
       handleFailToast("Erro ao enviar arquivo de produtos.");
-      
     } finally {
       setLoadingFile(false);
     }
@@ -99,146 +109,161 @@ const AddProductPage = () => {
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 px-4">
-      <Header/>
+      <Header />
       <div className="flex flex-1 items-center justify-center">
         <div className="w-full max-w-md p-6 bg-white rounded-2xl shadow-md">
           <h1 className="text-2xl font-bold text-gray-800 mb-4 self-center">
             Adicionar Produtos ao Catálogo do Almoxarifado
           </h1>
-          {/* Formulário para adicionar produto individualmente */}
-          <form onSubmit={handleFormSubmit} className="space-y-4">
-            <h2 className="text-xl font-semibold text-gray-700">
-              Adicionar Produto Individual
-            </h2>
-            <div>
-              <label
-                htmlFor="nome_produto"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Nome do Produto
-              </label>
-              <input
-                type="text"
-                id="nome_produto"
-                name="nome_produto"
-                value={formData.nome_produto}
-                onChange={handleChange}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Informe o nome do produto"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="codigo_produto"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Código do Produto
-              </label>
-              <input
-                type="text"
-                id="codigo_produto"
-                name="codigo_produto"
-                value={formData.codigo_produto}
-                onChange={handleChange}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Informe o código do produto"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="unidade"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Unidade de Medida (Opcional)
-              </label>
-              <input
-                type="text"
-                id="unidade"
-                name="unidade"
-                value={formData.unidade}
-                onChange={handleChange}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Informe a unidade"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="centro_custo"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Centro de Custo
-              </label>
-              <input
-                type="text"
-                id="centro_custo"
-                name="centro_custo"
-                value={formData.centro_custo}
-                onChange={handleChange}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Informe o centro de custo"
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className={`w-full py-2 px-4 text-white font-medium rounded-2xl shadow-sm focus:outline-none flex items-center justify-center ${
-                (formData.nome_produto && formData.codigo_produto && formData.centro_custo)
-                  ? "bg-blue-500 hover:bg-blue-600"
-                  : "bg-gray-400 cursor-not-allowed"
-              }`}
-              disabled={!formData.nome_produto || !formData.codigo_produto || loadingForm}
-            >
-              {loadingForm ? (
-                <>
-                  <LoadingSpinner message="Enviando..." />
-                </>
-              ) : (
-                "Adicionar Produto"
-              )}
-            </button>
-          </form>
+          <Accordion type="multiple" className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Adicionar Produto Individual</AccordionTrigger>
+              <AccordionContent>
+                {/* Formulário para adicionar produto individualmente */}
+                <form onSubmit={handleFormSubmit} className="space-y-4">
+                  <div>
+                    <label
+                      htmlFor="nome_produto"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Nome do Produto
+                    </label>
+                    <input
+                      type="text"
+                      id="nome_produto"
+                      name="nome_produto"
+                      value={formData.nome_produto}
+                      onChange={handleChange}
+                      className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                      placeholder="Informe o nome do produto"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="codigo_produto"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Código do Produto
+                    </label>
+                    <input
+                      type="text"
+                      id="codigo_produto"
+                      name="codigo_produto"
+                      value={formData.codigo_produto}
+                      onChange={handleChange}
+                      className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                      placeholder="Informe o código do produto"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="unidade"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Unidade de Medida (Opcional)
+                    </label>
+                    <input
+                      type="text"
+                      id="unidade"
+                      name="unidade"
+                      value={formData.unidade}
+                      onChange={handleChange}
+                      className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                      placeholder="Informe a unidade de medida"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="centro_custo"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Centro de Custo
+                    </label>
+                    <input
+                      type="text"
+                      id="centro_custo"
+                      name="centro_custo"
+                      value={formData.centro_custo}
+                      onChange={handleChange}
+                      className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                      placeholder="Informe o centro de custo"
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className={`w-full py-2 px-4 text-white font-medium rounded-2xl shadow-sm focus:outline-none flex items-center justify-center ${
+                      formData.nome_produto &&
+                      formData.codigo_produto &&
+                      formData.centro_custo
+                        ? "bg-blue-500 hover:bg-blue-600"
+                        : "bg-gray-400 cursor-not-allowed"
+                    }`}
+                    disabled={
+                      !formData.nome_produto ||
+                      !formData.codigo_produto ||
+                      loadingForm
+                    }
+                  >
+                    {loadingForm ? (
+                      <>
+                        <LoadingSpinner message="Enviando..." />
+                      </>
+                    ) : (
+                      "Adicionar Produto"
+                    )}
+                  </button>
+                </form>
+              </AccordionContent>
+            </AccordionItem>
 
-          <hr className="my-6" />
-
-          {/* Formulário para upload de arquivo Excel */}
-          <div>
-            <h2 className="text-xl font-semibold text-gray-700">
-              Adicionar Produtos via Excel
-            </h2>
-            <div className="space-y-4">
-              <div>
-                <p className="text-xs text-gray-500 mb-4">
-                  A planilha deve conter, pelo menos, quatro colunas seguindo o
-                  formato de nome "Código Produto ou Material", "Nome Produto ou Especificação", "Unidade" e "Centro de Custo".
-                </p>
-                <input
-                  type="file"
-                  accept=".xlsx"
-                  onChange={handleFileChange}
-                  className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none"
-                />
-              </div>
-              <button
-                onClick={handleFileSubmit}
-                className={`w-full py-2 px-4 text-white font-medium rounded-2xl shadow-sm focus:outline-none flex items-center justify-center ${
-                  file
-                    ? "bg-green-500 hover:bg-green-600"
-                    : "bg-gray-400 cursor-not-allowed"
-                }`}
-                disabled={!file || loadingFile}
-              >
-                {loadingFile ? (
-                  <>
-                    <LoadingSpinner message="Enviando..." />
-                  </>
-                ) : (
-                  "Upload de Arquivo"
-                )}
-              </button>
-            </div>
-          </div>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>Adicionar Produtos via Excel</AccordionTrigger>
+              <AccordionContent>
+                {/* Formulário para upload de arquivo Excel */}
+                <div>
+                  <h2 className="text-xl font-semibold text-gray-700">
+                    Adicionar Produtos via Excel
+                  </h2>
+                  <div className="space-y-4">
+                    <div>
+                      <p className="text-xs text-gray-500 mb-4">
+                        A planilha deve conter, pelo menos, quatro colunas
+                        seguindo o formato de nome "Código Produto ou Material",
+                        "Nome Produto ou Especificação", "Unidade" e "Centro de
+                        Custo".
+                      </p>
+                      <input
+                        type="file"
+                        accept=".xlsx"
+                        onChange={handleFileChange}
+                        className="mt-1 block w-full p-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none"
+                      />
+                    </div>
+                    <button
+                      onClick={handleFileSubmit}
+                      className={`w-full py-2 px-4 text-white font-medium rounded-2xl shadow-sm focus:outline-none flex items-center justify-center ${
+                        file
+                          ? "bg-green-500 hover:bg-green-600"
+                          : "bg-gray-400 cursor-not-allowed"
+                      }`}
+                      disabled={!file || loadingFile}
+                    >
+                      {loadingFile ? (
+                        <>
+                          <LoadingSpinner message="Enviando..." />
+                        </>
+                      ) : (
+                        "Upload de Arquivo"
+                      )}
+                    </button>
+                  </div>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       </div>
     </div>
